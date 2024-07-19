db497b5e8ee3ec10a3e07ec1c7bf29b6aa3a2341
jorgesosa-funval <jorgesosa@fundaciondevalores.org>
2024-07-19 10:29:48 -0400

    cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { HOST, PORT } from './config/ServerConfig.js';
 import { router } from './routes/index.js'
+import cors from 'cors'
 const app = express();
+app.use(cors())
 app.use(express.json())
 router(app)
 
